05a4537076f81ab8f9f69d7fee9e82b66148b3aa
Matthew Costabile <mattcosta7@gmail.com>
2016-01-20 17:48:51 -0500

    maybe?

--- app.rb
+++ app.rb
@@ -10,18 +10,3 @@ get '/' do
   erb :index
 end
 
-post '/email' do 
-  if ValidateEmail.valid?(params[:mail])
-    client = SendGrid::Client.new(api_user: ENV['SENDGRID_USERNAME'], api_key: ENV['SENDGRID_PASSWORD'])
-    email = SendGrid::Mail.new do |m|
-      m.to      = 'matt@mattc.io'
-      m.from    = params[:mail]
-      m.subject = 'Email Sent From Portfolio Contact Page'
-      m.text    = "Name: #{params[:name]}\r\n\r\nPhone: #{params[:number]}\r\n\r\nBody: #{params[:comment]}"
-    end
-    client.send(email)
-    redirect '/#contact', notice: "You've messaged me, I'll get back to you asap!"
-  else
-    redirect '/#contact', notice: "Please use a properly formatted email address!"
-  end
-end
