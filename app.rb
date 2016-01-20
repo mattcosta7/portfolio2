dc744e6c6f94c69ee0085067c5a4cb16406240ef
Matthew Costabile <mattcosta7@gmail.com>
2016-01-20 17:54:52 -0500

    maybe?

--- app.rb
+++ app.rb
@@ -1,7 +1,6 @@
 require 'sinatra'
 require 'sendgrid-ruby'
 require 'sinatra/flash'
-require 'sinatra/redirect_with_flash'
 require 'valid_email'
 require 'valid_email/validate_email'
 enable :sessions
@@ -10,3 +9,20 @@ get '/' do
   erb :index
 end
 
+post '/email' do 
+  if ValidateEmail.valid?(params[:mail])
+    client = SendGrid::Client.new(api_user: ENV['SENDGRID_USERNAME'], api_key: ENV['SENDGRID_PASSWORD'])
+    email = SendGrid::Mail.new do |m|
+      m.to      = 'matt@mattc.io'
+      m.from    = params[:mail]
+      m.subject = 'Email Sent From Portfolio Contact Page'
+      m.text    = "Name: #{params[:name]}\r\n\r\nPhone: #{params[:number]}\r\n\r\nBody: #{params[:comment]}"
+    end
+    client.send(email)
+    flash[:notice]="You've messaged me, I'll get back to you asap!"
+    redirect '/#contact'
+  else
+    flash[:notice]="Please use a properly formatted email address!"
+    redirect '/#contact'
+  end
+end
