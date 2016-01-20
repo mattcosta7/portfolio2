6cf22dbce321777204be1c8b35ebe755e6dc803e
Matthew Costabile <mattcosta7@gmail.com>
2016-01-20 17:29:45 -0500

    heroku?

--- app.rb
+++ app.rb
@@ -1,14 +1,10 @@
 require 'sinatra'
-require 'sinatra/bootstrap'
 require 'sendgrid-ruby'
 require 'sinatra/flash'
 require 'sinatra/redirect_with_flash'
 require 'valid_email'
 require 'valid_email/validate_email'
 enable :sessions
-register Sinatra::Bootstrap::Assets
-
-
 
 get '/' do 
   erb :index
