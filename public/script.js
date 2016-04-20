845fcca1840b3be86fbf9b4e5bbdd79427f36c12
Matthew Costabile <mattcosta7@gmail.com>
2016-04-20 09:08:17 -0400

    adds mapper

--- public/script.js
+++ public/script.js
@@ -3,51 +3,10 @@ $(document).ready(function(){
 
   $('.socialRoll').on('mouseenter',function(){
     $(this).find('span').show();
-  }) 
+  });
   $('.socialRoll').on('mouseleave',function(){
     $(this).find('span').hide();
-  })
+  });
 
 
-})
-
-onload=function(){
-
-  var nav = document.getElementsByTagName('nav')[0];
-  var body = document.getElementsByTagName('body')[0];
-  var social = document.getElementsByClassName('social')[0];
-  if (window.location.href == "http://www.mattc.io/resume" || window.location.href == "http://localhost:9494/resume"){
-    nav.style.display = 'block';
-    body.style.paddingTop = '100px';
-    social.style.marginTop = '-40px';
-  }
-  else if(body.scrollTop<60){
-    nav.style.display = 'none';
-    body.style.paddingTop = '20px';
-    social.style.marginTop = '40px';
-  }
-  else{
-    nav.style.display = 'block';
-    body.style.paddingTop = '20px';
-    social.style.marginTop = '40px';
-  }
-}
-
-function scroller(){
-  var nav = document.getElementsByTagName('nav')[0];
-  var body = document.getElementsByTagName('body')[0];
-  var social = document.getElementsByClassName('social')[0];
-  if (window.location.href == "http://www.mattc.io/resume" || window.location.href == "http://localhost:9494/resume"){
-   return;
-  }
-  else if(body.scrollTop > 60){
-    nav.style.display = 'block';
-    body.style.paddingTop = '20px';
-    social.style.marginTop = '40px';
-  }
-  else{
-    nav.style.display = 'none';
-    body.style.paddingTop = '20px';
-    social.style.marginTop = '40px';
-  }
-}
+});
